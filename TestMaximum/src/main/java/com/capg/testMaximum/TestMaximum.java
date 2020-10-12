ec4b905b91c11f8411bd48fab64dd8ae2a50d81d
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-12 19:15:10 +0530

    [deekshita] Add : Added welcome message

---
+++ TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
@@ -0,0 +1,15 @@
+package com.capg.testMaximum;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class TestMaximum 
+{	
+	private static Logger log=LogManager.getLogger(TestMaximum.class);
+    public static void main( String[] args )
+    {
+    	
+		log.info("Welcome");
+    }
+}
