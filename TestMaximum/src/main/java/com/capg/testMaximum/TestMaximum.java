88fc65f9b38b38a2d1d208a9a794593422e84680
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-12 23:03:28 +0530

    [dekshita] Add : Ability to add print function to print maximum of greater than 3 numbers

--- TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
+++ TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
@@ -5,26 +5,23 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-public class TestMaximum <T extends Comparable<T>>
-{
+public class TestMaximum<T extends Comparable<T>> {
 	List<T> inputs;
 	private static final Logger log = LogManager.getLogger(TestMaximum.class);
 	T x, y, z;
-	
-	public TestMaximum(T...inputs)
-	{
-		this.inputs=Arrays.asList(inputs);
+
+	public TestMaximum(T... inputs) {
+		this.inputs = Arrays.asList(inputs);
 	}
-	
-	public T toMax()
-	{
-		return  TestMaximum.toMax(inputs);
-		
+
+	public T toMax() {
+		T max = TestMaximum.toMax(inputs);
+		printMax(inputs, max);
+		return max;
+
 	}
-	
-	public static <T extends Comparable<T>> T toMax(List<T> inputs)
-	{
+
+	public static <T extends Comparable<T>> T toMax(List<T> inputs) {
 
 		T max;
 		List<T> sortedList = new ArrayList<>(inputs);
@@ -32,17 +29,17 @@ public class TestMaximum <T extends Comparable<T>>
 		max = sortedList.get(sortedList.size() - 1);
 		return max;
 	}
-	
-	
-	
-    public static void main( String[] args )
-    {
-    	
-		Integer a=40, b=20, c=55, d=2, e=43;
-		log.info(new TestMaximum<Integer>(a,b,c,d,e).toMax());
-		String m="45", n="60", o="55", p="6", q="21";
-		log.info(new TestMaximum<String>(m,n,o,p,q).toMax());
-		
-		
-    }
+
+	public static <T> void printMax(List<T> inputs, T max) {
+		log.info("Maximum among the list is " + max);
+	}
+
+	public static void main(String[] args) {
+
+		Integer a = 40, b = 20, c = 55, d = 2, e = 43;
+		new TestMaximum<Integer>(a, b, c, d, e).toMax();
+		String m = "apple", n = "orange", o = "banana", p = "grape", q = "mango";
+		new TestMaximum<String>(m, n, o, p, q).toMax();
+
+	}
 }
