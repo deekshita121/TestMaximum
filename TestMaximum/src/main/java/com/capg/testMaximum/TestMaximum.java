a9351a2f28c0d2c41d10327f51786e92ebe8c3b4
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-12 20:05:32 +0530

    [deekshita] Add : Ability to implement generic method to find maximum

--- TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
+++ TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
@@ -9,9 +9,9 @@ public class TestMaximum
 	
 	private static final Logger log = LogManager.getLogger(TestMaximum.class);
 	
-	public Integer toMax(Integer x, Integer y, Integer z)
+	public <T extends Comparable<T>> T toMax(T x, T y, T z)
 	{
-		Integer max = x;
+		T max = x;
 		if(y.compareTo(max)>0)
 			max=y;
 		if(z.compareTo(max)>0)
@@ -20,39 +20,14 @@ public class TestMaximum
 		return max;
 	}
 	
-	public Float toMax(Float x, Float y, Float z)
-	{
-		Float max = x;
-		if(y.compareTo(max)>0)
-			max=y;
-		if(z.compareTo(max)>0)
-			max=z;
 	
-		return max;
-	}
-
-	public String toMax(String x, String y, String z)
-	{
-		String max = x;
-		if(y.compareTo(max)>0)
-			max=y;
-		if(z.compareTo(max)>0)
-			max=z;
-	
-		return max;
-	}
 	
     public static void main( String[] args )
     {
     	TestMaximum testMaximum=new TestMaximum();
-		Integer a=40, b=20, c=25;
-		Float d=20.0f, e=13.f, f=21.f;
-		String m="15",n="32",o="21";
-		Integer maxInt=testMaximum.toMax(a, b, c);
-		Float maxFloat=testMaximum.toMax(d, e, f);
-		String maxString=testMaximum.toMax(m, n, o);
-		log.info("The maximum integer in "+a+", "+b+", "+c+" is "+maxInt);
-		log.info("The maximum float in "+d+", "+e+", "+f+" is "+maxFloat);
-		log.info("The maximum string in "+m+", "+n+", "+o+" is "+maxString);
+		String a="40", b="20", c="55";
+		String maxString=testMaximum.toMax(a, b, c);
+		log.info("The maximum integer in "+a+", "+b+", "+c+" is "+maxString);
+		
     }
 }
