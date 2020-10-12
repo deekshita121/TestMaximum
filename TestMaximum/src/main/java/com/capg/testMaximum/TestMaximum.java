e4d09e4571eedfdbce13768da6a0fc8de6650eb2
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-12 19:18:38 +0530

    [deekshita] Add : Ability to find maximum among 3 integers

--- TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
+++ TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
@@ -5,11 +5,27 @@ import org.apache.logging.log4j.Logger;
 
 
 public class TestMaximum 
-{	
-	private static Logger log=LogManager.getLogger(TestMaximum.class);
+{
+	
+	private static final Logger log = LogManager.getLogger(TestMaximum.class);
+	
+	public Integer toMax(Integer x, Integer y, Integer z)
+	{
+		Integer max = x;
+		if(y.compareTo(max)>0)
+			max=y;
+		if(z.compareTo(max)>0)
+			max=z;
+	
+		return max;
+	}
+
+	
     public static void main( String[] args )
     {
-    	
-		log.info("Welcome");
+    	TestMaximum testMaximum=new TestMaximum();
+		Integer a=40, b=20, c=25;
+		Integer maxInt=testMaximum.toMax(a, b, c);
+		log.info("The maximum integer in "+a+", "+b+", "+c+" is "+maxInt);
     }
 }
