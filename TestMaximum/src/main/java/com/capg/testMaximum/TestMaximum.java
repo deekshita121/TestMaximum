4f626120749ce5dfc8a96772a844c010f091c765
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-12 19:39:53 +0530

    [deekshita] Add : Ability to find maximum of 3 floats

--- TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
+++ TestMaximum/src/main/java/com/capg/testMaximum/TestMaximum.java
@@ -19,13 +19,27 @@ public class TestMaximum
 	
 		return max;
 	}
+	
+	public Float toMax(Float x, Float y, Float z)
+	{
+		Float max = x;
+		if(y.compareTo(max)>0)
+			max=y;
+		if(z.compareTo(max)>0)
+			max=z;
+	
+		return max;
+	}
 
 	
     public static void main( String[] args )
     {
     	TestMaximum testMaximum=new TestMaximum();
 		Integer a=40, b=20, c=25;
+		Float d=20.0f, e=13.f, f=21.f;
 		Integer maxInt=testMaximum.toMax(a, b, c);
+		Float maxFloat=testMaximum.toMax(d, e, f);
 		log.info("The maximum integer in "+a+", "+b+", "+c+" is "+maxInt);
+		log.info("The maximum float in "+d+", "+e+", "+f+" is "+maxFloat);
     }
 }
